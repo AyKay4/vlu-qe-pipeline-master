7d1b478c6d3dcca068b9d9ac00e7315eea300177
AyKay4 <nguyenthuannha44@gmail.com>
2026-09-21 13:51:59 +0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -19,7 +19,7 @@ function login(username, password) {
   }
 
   // 5. Kiểm tra thông tin đăng nhập đúng (Happy Path)
-  if (username === 'admin' && password === '9999') {
+  if (username === 'admin' && password === '123') {
     return true;
   }
 
